7a7195dcc460d287dabc34c501b2fc0af7a82e6e
Charles Leighton <chleighton1@gmail.com>
2024-01-13 17:14:10 -0500

    feat: add isheld styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import { nanoid } from "nanoid";
 import Die from "./Die";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
   function allNewDice() {
     const res = [];
     for (let x = 0; x < 10; x++) {
-      res.push(Math.floor(Math.random() * 5) + 1);
+      res.push({
+        value: Math.floor(Math.random() * 5) + 1,
+        isHeld: true,
+        id: nanoid(),
+      });
     }
     return res;
   }
 
-  const [dice, setDice] = useState(allNewDice());
-
   const diceElements = dice.map((die) => {
-    return <Die value={die} />;
+    return <Die key={die.id} value={die.value} isHeld={die.isHeld} />;
   });
 
   function handleClick() {

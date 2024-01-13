3c114a0cd01881df0a1c0e4bb9f068d04657e1ba
Charles Leighton <chleighton1@gmail.com>
2024-01-13 16:50:57 -0500

    feat: update game with roll dice btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import { useState } from "react";
 import "./App.css";
+import Die from "./Die";
 
 function App() {
+  function allNewDice() {
+    const res = [];
+    for (let x = 0; x < 10; x++) {
+      res.push(Math.floor(Math.random() * 5) + 1);
+    }
+    return res;
+  }
+
+  const [dice, setDice] = useState(allNewDice());
+
+  const diceElements = dice.map((die) => {
+    return <Die value={die} />;
+  });
+
+  function handleClick() {
+    setDice(allNewDice);
+  }
+
   return (
     <main>
-      <div className="main-ui"></div>
+      <div className="main-ui">
+        <div className="dice--container">{diceElements}</div>
+        <button className="roll--btn" onClick={handleClick}>
+          Roll
+        </button>
+      </div>
     </main>
   );
 }
